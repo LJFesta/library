b7002c548b32f30404847a3c03e593819d775a28
LJFesta <festa114@yahoo.co.uk>
2017-04-06 22:49:20 +0100

    Adding the bootstrap.groovy file

--- librarySystem/grails-app/init/librarysystem/BootStrap.groovy
+++ librarySystem/grails-app/init/librarysystem/BootStrap.groovy
@@ -3,6 +3,18 @@ package librarysystem
 class BootStrap {
 
     def init = { servletContext ->
+new Librarian (name:'Jane Doe', email:'JD@lib.com', office:'1234', username:'jd1234', password:'password', telephone:'12345678910', library:'Hallam').save()
+new Librarian (name:'John Doe', email:'JD1@lib.com', office:'12345', username:'jd12345', password:'password', telephone:'22345678910', library:'Hallam').save()
+new Library (name/building:'Hallam', address:'123 Howard Street', openingHours:'12.00', location:'adsetts', studySpaces:'123').save()
+new Library (name/building:'Colligeate', address:'123 Colligeate Cresent', openingHours:'12.00', location:'Heart of Campus', studySpaces:'123').save()
+new Book (title:'System', subject:'computing', author:'Mark Smith', isbn:'123456', dateBorrowed:'12/09/2017', returnDate:'13/09/2017', student:'Lee Festa', overdue:'0').save()
+new Book (title:'Architectures', subject:'computing', author:'Mark Smith', isbn:'1234567', dateBorrowed:'12/09/2017', returnDate:'13/09/2017', student:'Lee Festa', overdue:'0').save()
+new Student(name:'Lee Festa', email:'LF@lib.com', username:'LJFesta', password:'password', studentId:'1', course:'computing').save()
+new Student(name:'Jane Smith', email:'JS@lib.com', username:'JSmith', password:'password', studentId:'2', course:'computing').save()
+new Course(title:'computing', code:'123 abc', leader:'Lynne Dawson', department:'ACES', description:'Computing, admin and architecture', studyMode:'full time').save()
+new Course(title:'Games Software Development', code:'1234 abc', leader:'Mark Featherstone', department:'ACES', description:'Games software developpment and design', studyMode:'full time').save()
+new BookReview(book:'system', dateCreated:'12/09/2017', student:'Lee Festa', review:'Its been a long night').save()
+new BookReview(book:'architecture', dateCreated:'12/09/2017', student:'Lee Festa', review:'Its been a very long night').save()
     }
     def destroy = {
     }
